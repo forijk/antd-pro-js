895ac1431c842d2877b4b69380ba60092c852b88
yyj <yuyinjian@mafengwo.com>
2019-12-03 18:31:16 +0800

    Init.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  globals: {
+    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+    page: true,
+  },
+  rules: {
+    'max-len': ['error', { code: 300 }],
+    'no-nested-ternary': 0,
+    'no-console': 0,
+  },
+};
